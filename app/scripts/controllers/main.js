7ab8a56f8e456c2f529d8bba4e5568dd2fcd862e
Arnaud Prat <arnaud.prat@gmail.com>
2015-08-23 18:40:36 +0200

    Add movable items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,8 @@
 
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    $scope.todos = [];
+
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
