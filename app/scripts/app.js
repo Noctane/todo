c5744dbcb090ec0d315ec0ea179dc2eadb72d08d
Arnaud Prat <arnaud.prat@gmail.com>
2015-08-23 18:53:25 +0200

    Add local storage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,19 +16,20 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
+    'ui.sortable',
+    'LocalStorageModule'
   ])
+
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function(){
+      localStorageService.set('todos', $scope.todos);
+    }, true);
 
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
